9106ca562f2db4a89457e68932a093dea244e9b0
ahmarkhanpathan <ahmarkhan_p@yahoo.com>
2016-05-15 17:51:19 +0530

    Java Session4 assignment 1
    To print the reverse of an array

---
+++ src/package12/Array1.java
@@ -0,0 +1,26 @@
+package package12;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+
+	
+		// TODO Auto-generated method stub
+
+	
+
+		
+char ch[]={'s','t','r','i','n','g'};
+
+System.out.print("The array is ");
+System.out.println(ch);
+System.out.print("The reverse of the array is ");
+int i;
+   for(i =5; i>=0; i--)
+   {
+	   System.out.print(" "+(ch[i]));
+   }   
+
+
+	}
+}
